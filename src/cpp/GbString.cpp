92897580d4ebd1db9b83446cc0f6bed7c898a587
yangxu <yangxu@xugtek.com>
2024-09-06 17:58:20 +0800

    src/cpp: 增加GbChar类型，实现GB18030与UCS-1、UCS-2字符的互转换。

---
+++ src/cpp/GbString.cpp
@@ -0,0 +1,131 @@
+#include "GbString.hpp"
+
+namespace gb18030
+{
+	namespace v2022
+	{
+		namespace internal
+		{
+			extern uint32_t ucsToGbBmp[65535];
+			extern char32_t gbToUcsBmp[65535];
+			extern char32_t gbToUcsBmpExt[4][10][126][11];
+
+			using CharBytes = std::array<uint8_t, 4>;
+
+			//获取字节数组
+			inline CharBytes getCharBytes(uint32_t value) noexcept
+			{
+				//TODO: 默认小端 考虑大端？
+				return *reinterpret_cast<CharBytes*>(&value);
+			}
+
+			//字节数组转字符
+			inline uint32_t fromCharBytes(const uint8_t* data, size_t len) noexcept
+			{
+				//TODO: 默认小端 考虑大端？
+				if (len != 0)
+				{
+					if (len == 4)
+						return *reinterpret_cast<const uint32_t*>(data);
+
+					CharBytes bytes = getCharBytes(0);
+					bytes[0] = data[0];
+					
+					if (len > 1)
+						bytes[1] = data[1];
+					if (len > 2)
+						bytes[2] = data[2];
+					
+					return *reinterpret_cast<uint32_t*>(&bytes);
+				}
+				return 0;
+			}
+
+			//获取GB18030字符占用字节数
+			inline size_t getGbCharSize(uint32_t c) noexcept
+			{
+				if (c <= 0x7F)
+					return 1;
+				else if (c <= 0xFFFF)
+					return 2;
+				return 4;
+			}
+
+			//获取GB18030字符边界
+			inline size_t getGbCharBound(const uint8_t* data, size_t len) noexcept
+			{
+				if (len != 0)
+				{
+					if (data[0] <= 0x7F)
+					{
+						return 1;
+					}
+					else if (0x7F < data[1] || 0x40 <= data[1] && data[1] <= 0x7E)
+					{
+						return 2;
+					}
+					else if (0x30 <= data[1] && data[1] <= 0x39 && 0x7F < data[2] && 0x30 <= data[3] && data[3] <= 0x39)
+					{
+						return 4;
+					}
+				}
+				return 0;
+			}
+
+			inline uint32_t unicodeToGb(char32_t c) noexcept
+			{
+				if (c <= 0x7F)  //单字节区 等同
+					return (uint32_t)c;
+				else if (c <= 0xFFFF)  //双字节区 BMP
+					return ucsToGbBmp[c];
+				else if (c <= 0x10FFFF)  //四字节区 SMP
+					return c;  //TODO: implement
+
+				return 0;  //非法字符
+			}
+
+			inline char32_t gbToUnicode(uint32_t c) noexcept
+			{
+				auto size = getGbCharSize(c);
+				if (size == 1)
+					return (char32_t)c;
+
+				if (size == 2)
+					return gbToUcsBmp[c];
+
+				if (size == 4)
+				{
+					//TODO: implement
+				}
+
+				return 0;  //非法字符
+			}
+		}
+
+		size_t GbChar::size() const noexcept
+		{
+			return internal::getGbCharSize(char_);
+		}
+
+		GbChar GbChar::fromUnicode(char32_t u32) noexcept
+		{
+			return GbChar{ internal::unicodeToGb(u32) };
+		}
+
+		char32_t GbChar::toUnicode() const noexcept
+		{
+			return internal::gbToUnicode(char_);
+		}
+
+		GbChar GbChar::fromBytes(const char* data, size_t len) noexcept
+		{
+			auto bound = internal::getGbCharBound((const uint8_t*)data, len);
+			return GbChar{ internal::fromCharBytes((const uint8_t*)data, bound) };
+		}
+
+		std::array<uint8_t, 4> GbChar::toBytes() const noexcept
+		{
+			return internal::getCharBytes(char_);
+		}
+	}
+}
--- src/cpp/GbString.hpp
+++ src/cpp/GbString.hpp
@@ -1,13 +1,155 @@
 #pragma once
 
+#include <array>
+#include <string>
+#include <vector>
+#include <cstddef>
+#include <cstdint>
+
 namespace gb18030
 {
 	inline namespace v2022
 	{
+		// GB18030字符
+		class GbChar
+		{
+		public:
+			// 初始化为空字符
+			GbChar() noexcept
+				: char_(0)
+			{}
+
+			// 从GB18030编码数值得到GB18030字符
+			GbChar(uint32_t gbCharValue) noexcept
+				: char_(gbCharValue)
+			{}
+
+			~GbChar() noexcept = default;
+
+			GbChar(const GbChar& other) noexcept = default;
+			GbChar& operator=(const GbChar& right) noexcept = default;
+
+			GbChar(GbChar&& other) noexcept = default;
+			GbChar& operator=(GbChar&& right) noexcept = default;
+
+			// 判断是否空字符
+			bool isNul() const noexcept
+			{
+				return char_ == 0;
+			}
+
+			// 获取字符字节大小
+			size_t size() const noexcept;
+
+			// 从Unicode字符得到GB18030字符
+			static GbChar fromUnicode(char32_t u32) noexcept;
+
+			// 转换到Unicode字符
+			char32_t toUnicode() const noexcept;
+
+			// 从字节流获取字符
+			//
+			// 仅获取第一个字符
+			static GbChar fromBytes(const char* data, size_t len) noexcept;
+
+			// 获取字符字节
+			std::array<uint8_t, 4> toBytes() const noexcept;
+
+			// 获取数值
+			uint32_t value() const noexcept
+			{
+				return char_;
+			}
+			
+		private:
+			uint32_t char_;
+		};
+
+
 		// GB18030字符串
+		//
+		// GB18030是1、2、4变长字符编码 这里为了方便编辑采用固定4字节存储
+		// fromBytes和toBytes都是变长字节流
 		class GbString
 		{
-			//TODO: 实现
+		public:
+			// 适配容器接口
+			using Data = std::vector<GbChar>;
+			using size_type = Data::size_type;
+			using value_type = Data::value_type;
+			using reference = Data::reference;
+			using const_reference = Data::const_reference;
+			using iterator = Data::iterator;
+			using const_iterator = Data::const_iterator;
+			using reverse_iterator = Data::reverse_iterator;
+			using const_reverse_iterator = Data::const_reverse_iterator;
+
+			// 空字符串
+			GbString() noexcept = default;
+
+			GbString(const GbString& other) = default;
+			GbString& operator=(const GbString& right) = default;
+
+			GbString(GbString&& other) noexcept = default;
+			GbString& operator=(GbString&& right) = default;
+
+			void swap(GbString& other) noexcept
+			{
+				data_.swap(other.data_);
+			}
+
+			// 检查字符串是否为空
+			bool empty() const noexcept
+			{
+				return data_.empty();
+			}
+
+			// 获取字符串长度
+			//
+			// 字符串的字符数量
+			size_type length() const noexcept
+			{
+				return data_.size();
+			}
+
+			// 获取指定位置字符
+			reference at(size_type index)
+			{
+				return data_.at(index);
+			}
+
+			// 获取指定位置字符
+			const_reference at(size_type index) const
+			{
+				return data_.at(index);
+			}
+
+			// 获取指定位置字符
+			reference operator[](size_type index) noexcept
+			{
+				return data_[index];
+			}
+
+			// 获取指定位置字符
+			const_reference operator[](size_type index) const noexcept
+			{
+				return data_[index];
+			}
+
+			// 从Unicode字符串获取GB18030字符串
+			static GbString fromUnicode(const char32_t* data, size_t len);
+
+			// 转换到Unicode字符串
+			std::u32string toUnicode() const;
+
+			// 从字节流获取GB18030字符串
+			static GbString fromBytes(const char* data, size_t len);
+
+			// 转换到GB18030字节流
+			std::string toBytes() const;
+
+		private:
+			Data data_;
 		};
 	}
 }
