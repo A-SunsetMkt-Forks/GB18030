5928e2df1b9535991f5cd6901f02dd1099f1490d
yangxu <yangxu@xugtek.com>
2024-09-09 14:24:36 +0800

    src/cpp: 增加GbString测试用例。

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 using namespace gb18030::v2022;
 
+#define TEST_BEG cout << __FUNCTION__ << "\n";
+#define TEST_END cout << "\n\n";
+
 void testGbCharBytes()
 {
-	cout << __FUNCTION__ << "\n";
+	TEST_BEG
 
 	GbChar c = GbChar::fromBytes("\xB0\xA1", 2);
 	cout << "gb char B0A1 hex: " << hex << c.value() << "\n";
@@ -14,12 +17,13 @@ void testGbCharBytes()
 	cout << "to bytes: ";
 	for (size_t i = 0; i < c.size(); ++i)
 		cout << hex << (int)bs[i] << " ";
-	cout << endl;
+	
+	TEST_END
 }
 
 void testGbCharUnicode()
 {
-	cout << __FUNCTION__ << "\n";
+	TEST_BEG
 
 	GbChar c1 = GbChar::fromUnicode(U'汉');
 	cout << "unicode to gb hex: " << hex << c1.value() << "\n";
@@ -27,6 +31,68 @@ void testGbCharUnicode()
 	GbChar c2 = GbChar::fromBytes("\xBA\xBA", 2);
 	auto uc = c2.toUnicode();
 	cout << "gb to unicode hex: " << hex << uc << "\n";
+
+	TEST_END
+}
+
+void testGbStringBasic()
+{
+	TEST_BEG
+
+	GbString s;
+	s.push_back(GbChar::fromUnicode(U'中'));
+	s.push_back(GbChar::fromUnicode(U'文'));
+
+	cout << "length: " << s.length() << " bytes: ";
+
+	for (auto ch : s)
+	{
+		auto size = ch.size();
+		auto bytes = ch.toBytes();
+		for (size_t i = 0; i < size; ++i)
+			cout << hex << int(bytes[i]);
+		cout << " ";
+	}
+
+	TEST_END
+}
+
+void testGbStringUnicode()
+{
+	TEST_BEG
+
+	GbString s = GbString::fromUnicode(U"中国中文汉字", 6);
+	cout << "length: " << s.length() << " bytes: ";
+
+	for (auto ch : s)
+	{
+		auto size = ch.size();
+		auto bytes = ch.toBytes();
+		for (size_t i = 0; i < size; ++i)
+			cout << hex << int(bytes[i]);
+		cout << " ";
+	}
+
+	TEST_END
+}
+
+void testGbStringBytes()
+{
+	TEST_BEG
+
+	GbString s = GbString::fromBytes("\xD6\xD0\xB9\xFA\xD6\xD0\xCE\xC4\xBA\xBA\xD7\xD6 abc", 16);
+	cout << "length: " << dec << s.length() << " bytes: ";
+
+	for (auto ch : s)
+	{
+		auto size = ch.size();
+		auto bytes = ch.toBytes();
+		for (size_t i = 0; i < size; ++i)
+			cout << hex << int(bytes[i]);
+		cout << " ";
+	}
+
+	TEST_END
 }
 
 void example()
@@ -38,5 +104,8 @@ int main()
 {
 	testGbCharBytes();
 	testGbCharUnicode();
+	testGbStringBasic();
+	testGbStringUnicode();
+	testGbStringBytes();
 	return 0;
 }
